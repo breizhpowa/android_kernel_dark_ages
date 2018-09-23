1aff6b5a323862bc57821522c498376231d45550
Blacksuan19 <Abubakaryagob@gmail.com>
2018-09-23 14:30:57 +0800

    boeffla_wakelock_blocker: fix bootloop
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -19,5 +19,5 @@
 #define LIST_WL_DEFAULT			"IPA_WS;NETLINK;netmgr_wl;qcom_rx_wakelock;[timerfd];wcnss_filter_lock;wlan;wlan_extscan_wl;wlan_ipa;wlan_pno_wl;wlan_wow_wl;ipc000000ec_system_server;dsps_IPCRTR;wcnss_filter_lock"
 
 #define LENGTH_LIST_WL			255
-#define LENGTH_LIST_WL_DEFAULT		125
+#define LENGTH_LIST_WL_DEFAULT		113
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
